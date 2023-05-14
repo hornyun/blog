da8b6275a3e8b834efe83f59e47072f35144f3b0
hornYun <hornyun@sina.com>
2023-05-14 15:40:26 +0800

    create blog framework

---
+++ readMe.md
@@ -0,0 +1,10 @@
+### 个人博客搭建
+
+#### 技术栈
+* springboot
+* mybatis-plus
+* vue3.0
+
+#### 参考资料
+* [druid wiki](https://github.com/alibaba/druid/wiki/%E5%B8%B8%E8%A7%81%E9%97%AE%E9%A2%98)
+* [springboot下使用druid-spring-boot-starter](https://blog.csdn.net/yzh_1346983557/article/details/117673280)
---
+++ src/main/java/com/hornyun/blog/BlogApplication.java
@@ -0,0 +1,17 @@
+package com.hornyun.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hornyun
+ * created on 2023/05/14
+ */
+@SpringBootApplication
+public class BlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class, args);
+    }
+
+}
